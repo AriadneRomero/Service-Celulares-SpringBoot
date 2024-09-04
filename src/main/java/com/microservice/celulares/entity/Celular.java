ad76842e87f229347918b41e41e64ad0856fb807
Ariadne Romero <88512668+AriadneRomero@users.noreply.github.com>
2024-09-04 01:09:12 -0600

    Video #1 Update

--- src/main/java/com/microservice/celulares/entity/Celular.java
+++ src/main/java/com/microservice/celulares/entity/Celular.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-
+// Ariadne Romero
 @Entity
 @Table(name = "celulares")
 public class Celular implements Serializable{
